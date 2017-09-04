2c53adde2e03a6f4c073aa4f043ee28dc653845c
Vivek Saini <vivek-1000@hotmail.com>
2017-09-04 00:29:19 -0400

    Renamed rem-kill-chrome to rem-chrome-kill to make easier to remember. Also modified rem-chrome to kill existing chrome processes before executing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rem-chrome
 A wrapper for nohup to open chrome over ssh
+
+Make sure to source the rem-chrome.sh file before attempting to use the functions
+
--- rem-chrome.sh
+++ rem-chrome.sh
@@ -1,13 +1,15 @@
 # Takes in a user, ip address and browser address and remotely opens chrome using nohup to the page pointed by the browser address
 # on the machine at the ip address using the ssh protocol
 
-rem-chrome () {
-	ssh $1@$2 DISPLAY=:0 nohup google-chrome $3 1>/dev/null 2>&1 &	
-}
-
-rem-kill-chrome () {
+rem-chrome-kill () {
 	pid=$(ssh $1@$2 ps -f | grep http | awk '{print$2}')
 	echo $1, $2
 	echo $pid
 	ssh $1@$2 DISPLAY=:0 nohup kill $pid 1>/dev/null 2>&1 &
 }
+
+rem-chrome () {
+	rem-chrome-kill $1 $2
+	ssh $1@$2 DISPLAY=:0 nohup google-chrome $3 1>/dev/null 2>&1 &	
+}
+
