fd063d5bea19ea4aeb7cbe51f9abcb7c813060ce
Vivek Saini <vivek-1000@hotmail.com>
2017-09-04 02:03:51 -0400

    Added environment variables for username and ip address and a function to set them. Defaults to my current home network. Makes command input far less archaic.

--- rem-chrome.sh
+++ rem-chrome.sh
@@ -1,15 +1,26 @@
 # Takes in a user, ip address and browser address and remotely opens chrome using nohup to the page pointed by the browser address
 # on the machine at the ip address using the ssh protocol
 
+ip=192.168.1.2
+user=bobochimpus
+
+rem-set-user-and-ip () {
+	user=$1
+	ip=$2
+}
+
 rem-chrome-kill () {
-	pid=$(ssh $1@$2 ps -f | grep http | awk '{print$2}')
+	pid=$(ssh $user@$ip ps -f | grep http | awk '{print$2}')
 	echo $1, $2
 	echo $pid
-	ssh $1@$2 DISPLAY=:0 nohup kill $pid 1>/dev/null 2>&1 &
+	ssh $user@$ip DISPLAY=:0 nohup kill $pid 1>/dev/null 2>&1 &
 }
 
 rem-chrome () {
-	rem-chrome-kill $1 $2
-	ssh $1@$2 DISPLAY=:0 nohup google-chrome $3 1>/dev/null 2>&1 &	
+	rem-chrome-kill $user $ip
+	ssh $user@$ip DISPLAY=:0 nohup google-chrome $1 1>/dev/null 2>&1 &	
 }
 
+rem-vol-adj () {
+	ssh $user@$ip DISPLAY=:0 nohup amixer -D pulse sset Master $1 1>/dev/null 2>&1 &
+}
